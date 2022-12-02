d0a8b5648f12176271821534d483b3408b501a89
xuaanquynh04 <xuaanquynh04@gmail.com>
2022-12-02 08:04:43 +0700

    bản final ngay 2/12 có note

--- caro.py
+++ caro.py
@@ -37,7 +37,6 @@ def hien(so, p): #này  để hiển thị bàn cờ caro
     for i in range(0, 8, 3):
         c = str(ca[i]) + " " + str(ca[i + 1]) + " " + str(ca[i + 2])
         print(c)
-    return ca
 c = hien(so, p)
 while flag == 1: #này là cho người chơi nhập vào, gọi các hàm ở trên ra cho nó gọn
     a=int(input("nguoi choi 1 muon danh O vao o nao "))
